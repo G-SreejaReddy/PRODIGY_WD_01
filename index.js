f4388b374108f4802dc2f000bf7c528d152f2ea6
G-SreejaReddy <sreejareddi@gamil.com>
2024-05-25 11:30:10 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', function() {
+    var navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.style.backgroundColor = black;
+    } else {
+        navbar.style.backgroundColor = '#006400';
+    }
+});
